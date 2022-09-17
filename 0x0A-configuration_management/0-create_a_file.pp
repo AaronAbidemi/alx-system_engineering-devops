570ec3d5c4127d87015a6a40d37b1faeba28527b
Aaron Abidemi <99512094+AaronAbidemi@users.noreply.github.com>
2022-09-17 21:46:47 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #!/usr/bin/pup
 # This manifests changes details of file
-file { '/tmp/school': 
-ensure => file, 
-owner => 'www-data', 
-group => 'www-data', 
-mode => '0744', 
-content => 'I love Puppet',
+file { '/tmp/school':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
 }
